51ae2c303994e4eff324bcfef4d1c6d7ad186d1b
Giorgi Maziashvili <gmaziashvili@live.com>
2018-12-23 01:18:46 +0400

    First commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public float speed;
+    public float upForce;
+
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    private Rigidbody2D rigidbody;
+
+	void Start () {
+
+        speed = 3f;
+        upForce = 5f;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+	}
+
+   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name=="door")
+        {
+          //  Transpose();
+        }
+    }
+
+    void Update () {
+        Vector3 move =new Vector3(Input.GetAxis("Horizontal"),0, 0);
+        transform.position += move * speed * Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        { 
+            spriteRenderer.flipX = true;
+            animator.SetBool("walk", true);
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animator.SetBool("walk", false);
+            rigidbody.AddForce(new Vector2(0, upForce), ForceMode2D.Impulse);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetBool("walk", true);
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    public GameObject FirstGameObject;
+    public GameObject SecondGameObject;
+
+    public Camera camera1;
+    public Camera camera2;
+
+    Character c1,c2;
+
+    private void Start()
+    {
+        c1 = FirstGameObject.GetComponent<Character>();
+        c2 = SecondGameObject.GetComponent<Character>();
+
+        //////////////////////////////////////////////
+
+        camera1.enabled = true;
+        camera2.enabled = false;
+
+        c1.enabled = true;
+        c2.enabled = false;
+    }
+
+
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            FocusToBoth();
+        }
+		if(Input.GetKeyDown(KeyCode.V))
+        {
+            FocusToSecond();
+        }
+	}
+
+
+    private void FocusToBoth()
+    {
+        c1.enabled = true;
+        c2.enabled = true;
+    }
+
+    public void FocusToSecond()
+    {
+        
+
+
+        if(c1.enabled&&c2.enabled)
+        {
+            c1.enabled = true;
+            camera1.enabled = true;
+
+            c2.enabled = false;
+            camera2.enabled = false;
+        }
+        else
+        {
+            c1.enabled = !c1.enabled;
+            c2.enabled = !c2.enabled;
+
+            camera1.enabled = !camera1.enabled;
+            camera2.enabled = !camera2.enabled;
+        }
+
+        
+    }
+    
+}
